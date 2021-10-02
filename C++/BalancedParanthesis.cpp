8ae674445949a6401323cc13066e6955355f64df
Yashcode007 <91584919+Yashcode007@users.noreply.github.com>
2021-10-02 11:44:32 +0530

    Update BalancedParanthesis.cpp
    This code is much more convinient and less number of functions and loop are applied on this piece of code.

--- C++/BalancedParanthesis.cpp
+++ C++/BalancedParanthesis.cpp
@@ -1,62 +1,58 @@
 #include <iostream>
-#include <stack>
-#include <string>
+#include<stack>
+
 using namespace std;
 
-bool check(char x, char y)
-{
-	if ((x=='(' && y==')') || (x=='{' && y=='}') || (x=='[' && y==']') || (x=='<' && y=='>'))
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+void isBalanced(string s){
+    stack<char> st ;
+    int n= s.size() ;
+    for(int i=0; i<n ; i++){
+        if(s[i]=='{' or s[i]=='[' or s[i]=='(' ){
+            st.push(s[i]);
+        }
+        else if(s[i]==')'){
+            if(st.top()=='('){
+                st.pop();
+                continue ;
+            }
+            else {
+                cout<<"Unbalanced Paranthesis" ;
+                break ;
+            }
+        }
+        else if(s[i]=='}'){
+            if(st.top()=='{'){
+                st.pop();
+                continue ;
+            }
+            else {
+                cout<<"Unbalanced Paranthesis" ;
+                break ;
+            }
+        }
+        else if(s[i]==']'){
+            if(st.top()=='['){
+                st.pop();
+                continue ;
+            }
+            else {
+                cout<<"Unbalanced Paranthesis" ;
+                break ;
+            }
+        }
+    }
+    if(st.size()==0){
+        cout<<"Balanced Paranthesis";
+    }
+    else{
+    cout<<"Unbalanced Paranthesis";
+    }
 }
 
-bool checkbalancedparenthesis(string s)
-{
-	stack<char> st;
-	int top=-1;
-	int l=s.length();
-	for(int i=0;i<l;i++)
-	{
-		if(s[i]=='(' || s[i]=='{' || s[i]=='[')
-		{
-		st.push(s[i]);
-		top=top+1;
-		}
-		else if(s[i]==')' || s[i]=='}' || s[i]==']')
-		{
-			if(top==-1 || check(st.top(),s[i])==false)
-			{
-				return false;
-			}
-			else
-			{
-			st.pop();
-			top=top-1;
-			}
-		}
-	}
-	if(top==-1)
-	return true;
-	else
-	return false;
+
+
+int main(){
+    string s="{[()]}" ;
+    isBalanced(s) ;
 }
-int main() 
-{
-	int test;
-	cin>>test;
-	while(test-->0)
-	{
-	string s;
-	cin>>s;
-	if(checkbalancedparenthesis(s)==true)
-	cout<<"BALANCED"<<endl;
-	else
-	cout<<"NOT BALANCED"<<endl;
-	}
-	return 0;
-}
+
